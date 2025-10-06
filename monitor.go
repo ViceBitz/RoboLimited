631e4fe732176ad72562e077a94e650b71d471f0
Victor Gong <141887299+ViceBitz@users.noreply.github.com>
2025-10-05 23:10:37 -0400

    restore deleted files

---
+++ monitor.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"log"
+	"math"
+	"robolimited/config"
+	"robolimited/tools"
+	"strconv"
+	"time"
+)
+
+
+// Evaluates if margins are good enough to buy
+func BuyF(rap_margin float64, value_margin float64, hasValue bool, isDemand bool) bool {
+	//Implement demand evaluation (higher demand items have lower margin standards)
+	if isDemand {
+		if hasValue {
+			return value_margin >= config.ValueDipD
+		}
+		return rap_margin >= config.RAPDipD
+	} else {
+		if hasValue {
+			return value_margin >= config.ValueDipND
+		}
+		return rap_margin >= config.RAPDipND
+	}
+}
+
+// Make decision on whether to buy or stand
+func BuyCheck(bestPrice int, RAP_r int, value_r int, isDemand bool) bool {
+	if bestPrice == 0 { //Error occurred or no resellers if price is 0
+		return false
+	}
+	RAP := float64(RAP_r)
+	value := float64(value_r)
+	bpF := float64(bestPrice)
+	if value == -1 {
+		//RAP limited
+		return BuyF((RAP-bpF)/RAP, -1, false, isDemand)
+	} else {
+		//Value limited
+		return BuyF((RAP-bpF)/RAP, (value-bpF)/value, true, isDemand)
+	}
+}
+
+// Monitor limited deals via Rolimon's deals page
+func monitorDeals(live_money bool) {
+
+	var tradeSim *tools.TradeSimulator = tools.NewTradeSimulator()
+
+	//id -> [item_name, acronym, rap, value, default_value, demand, trend, projected, hyped, rare]
+	itemDetails := tools.GetLimitedData()
+
+	RAP_map := map[string]int{}
+
+	for i := range config.TotalIterations {
+		log.Println("____________________________________________________")
+		if i%config.RefreshRate == 0 {
+			//Recalculate RAP / Value and limited data from Rolimon API
+			itemDetailsNew := tools.GetLimitedData()
+			if itemDetailsNew == nil {
+				//Mark errors in updating
+				log.Println("Could not refresh item details..")
+			} else {
+				itemDetails = itemDetailsNew
+			}
+		}
+
+		//[[timestamp, isRAP, id, bestPrice / RAP]]
+		dealDetails := tools.GetDealsData()
+		if dealDetails == nil {
+			continue
+		} //Catch error, wait for resolution
+
+		activities := dealDetails.Activities
+
+		for _, info := range activities {
+			isRAP := int(info[1].(float64))
+			id_r := int(info[2].(float64))
+			id := strconv.Itoa(id_r)
+			price := int(info[3].(float64))
+
+			//Handle not found error
+			if itemDetails == nil || len(itemDetails.Items[id]) == 0 {
+				continue
+			}
+
+			demand := int(itemDetails.Items[id][5].(float64))
+			projected := int(itemDetails.Items[id][7].(float64))
+
+			//Exclude projected items
+			if projected != -1 {
+				continue
+			}
+			//Exclude items out of price range
+			if !(config.PriceRangeLow <= price && price <= config.PriceRangeHigh) {
+				continue
+			}
+
+			//Scan for item details
+			name := itemDetails.Items[id][0].(string)
+			value := int(itemDetails.Items[id][3].(float64))
+			_, inMap := RAP_map[id]
+			if !inMap {
+				RAP_map[id] = int(itemDetails.Items[id][2].(float64))
+			}
+
+			if isRAP == 0 { //Updating best price
+				//Make decision to buy/sell
+
+				//Check buys
+				if BuyCheck(price, RAP_map[id], value, demand != -1) {
+					//Final price manipulation check
+					if !(config.DeepManipulationCheck && CheckProjected(id, float64(RAP_map[id]))) {
+						//Final dip check (if strict buy conditions)
+						if !config.StrictBuyCondition || CheckDip(id, float64(price)) {
+							//BUY
+							if !live_money {
+								tradeSim.BuyItem(id, name, price)
+							} else {
+								tradeSim.BuyItem(id, name, price)
+								ExecutePurchase(id, false)
+							}
+						}
+					}
+				}
+
+				log.Println("Scanned", name, "|", "RAP:", RAP_map[id], "| Value:", value, "| Price: ", price, "| Deal: ", math.Round(float64(max(RAP_map[id], value)-price)/float64(max(RAP_map[id], value))*1000.0)/10.0, "%")
+
+			} else { //Updating RAP
+				RAP_map[id] = price
+
+				log.Println("Updated", name, "|", "RAP:", RAP_map[id], "| Value:", value, "| Price: ", price)
+			}
+		}
+		time.Sleep(time.Millisecond * 1000)
+	}
+}
+
+//Driver
+func main() {
+	//Start deal sniper process
+	monitorDeals(config.LiveMoney)
+
+	//Analyzer Methods
+	//SearchFallingItems(-0.5, 1700, 1900, false) //Finds price-lowering items in market
+	//log.Println(FindOptimalSell("1301384400")) //Calculates optimal selling price
+
+	//Order executor test
+	//ExecutePurchase("331486631", true)
+}
