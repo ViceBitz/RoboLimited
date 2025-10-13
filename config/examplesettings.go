9a4ea62f37c5aabb569c4524ddb1c2d45ec85dea
ViceBitz <25victorg@students.harker.org>
2025-10-13 14:51:13 -0400

    add comment

--- config/examplesettings.go
+++ config/examplesettings.go
@@ -59,6 +59,6 @@ const (
 	UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
 
 	//Logging
-	LogConsole = false
+	LogConsole = false //Toggle print for processes & stats during execution
 )
 */
